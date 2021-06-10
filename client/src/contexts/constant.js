1bae47a97aaa6ccd4a79df11077fdd046a16cd25
lyminhnhi <67637831+lyminhnhi@users.noreply.github.com>
2021-06-10 10:54:18 +0700

    Prepare deploy

--- client/src/contexts/constant.js
+++ client/src/contexts/constant.js
@@ -1,7 +1,7 @@
 export const apiUrl =
 	process.env.NODE_ENV !== 'production'
 		? 'http://localhost:5000/api'
-		: 'something'
+		: 'https://secure-bayou-76978.herokuapp.com/api'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'minhnhi-my-udemy'
 
